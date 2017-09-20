1ef1ba88aa58caadeb0576de3f8bcccc74e8e4f5
Lava Kumar Bokam <lava.kumar@juspay.in>
2017-09-21 00:35:27 +0530

    added polybar config

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ ln -sf dotfiles/emacs.d ~/.emacs.d
 ln -sf dotfiles/vimperator ~/.vimperator
 ln -sf dotfiles/vimperator/vimperatorrc ~/.vimperatorrc
 
+ln -sf dotfiles/polybar ~/.polybar 
+
 ln -sf dotfiles/i3 ~/.i3
-rm -rf ~/.zshrc
-ln -sf dotfiles/zshrc ~/.zshrc
