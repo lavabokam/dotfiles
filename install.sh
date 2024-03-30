5e078ea5ef3a21d0527f879aaafb109f44bf00d8
lava <lavajnv@gmail.com>
2024-03-30 08:46:48 +0530

    moving older config to archive

--- install.sh
+++
@@ -1,5 +0,0 @@
-ln -sf dotfiles/emacs.d ~/.emacs.d
-ln -sf dotfiles/xmonad ~/.xmonad
-ln -sf dotfiles/X/Xresources ~/.Xresources
-ln -sf dotfiles/X/xinitrc ~/.xinitrc
-ln -sd dotfiles/X/Xmodmap ~/.Xmodmap
